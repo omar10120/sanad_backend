e65a972f46211a6504d387910528e26d13f6108e
tamer tamer <amr.dawoodi@hotmail.com>
2026-07-10 14:33:40 +0300

    remove star from edit teacher

--- resources/views/setting/teacher/teachers.blade.php
+++ resources/views/setting/teacher/teachers.blade.php
@@ -266,7 +266,7 @@
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label class="col-md-4 col-form-label text-md-end">{{ trans('main_trans.Estimation_time') }} <span class="tx-danger">*</span></label>
+                            <label class="col-md-4 col-form-label text-md-end">{{ trans('main_trans.Estimation_time') }} </label>
                             <div class="col-md-8">
                                 <input class="form-control" name="estimation_time" id="edit_estimation_time" type="number" min="0" >
                             </div>
